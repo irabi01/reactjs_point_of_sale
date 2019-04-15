253ae88b6bfbad8b334b742c988d077cfc4b4757
irabi <irabikassim01@gmail.com>
2019-04-15 08:50:18 +0300

    style setting component

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Setting extends Component {
   render() {
     return (
-      <div>
+      <div className="setting_container">
+        <div className="container">
         <h1>Setting</h1>
+          <div className="row">
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  register user
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Database Backup
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Database Restore
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Create POS
+                </div>
+              </Link>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  System logs
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Assign users to POS
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Assign staff role
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <Link className="link">
+                <div className="content">
+                  Tender types
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
